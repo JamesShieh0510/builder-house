e2780b1aaf72c8c934c7574e5a2f20494e0bae73
James Shieh <jamesshieh0510@gmail.com>
2024-11-15 19:54:55 +0800

    modify next.config.js

--- next.config.js
+++ next.config.js
@@ -1,9 +1,20 @@
 /** @type {import('next').NextConfig} */
 const { i18n } = require("./next-i18next.config");
 
-const nextConfig = {
-  reactStrictMode: true,
-  i18n,
-};
+// const nextConfig = {
+//   reactStrictMode: true,
+//   i18n,
+// };
 
 module.exports = nextConfig;
+
+const path = require('path');
+
+module.exports = {
+  reactStrictMode: true,
+  i18n,
+  webpack: (config) => {
+    config.resolve.alias['@'] = path.resolve(__dirname, 'src');
+    return config;
+  },
+};
