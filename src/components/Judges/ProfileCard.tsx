afed7bd2231381a18616a6a79594c8a6f5f853e4
Jason <chengyen030@gmail.com>
2024-11-22 22:36:37 +0800

    modify speakers image

--- src/components/Judges/ProfileCard.tsx
+++ src/components/Judges/ProfileCard.tsx
@@ -1,5 +1,4 @@
 import React from "react";
-import SvgIcon from "@/components/SvgIcon/SvgIcon";
 import { Box } from "@chakra-ui/react";
 
 export default function ProfileCard(props: {
@@ -15,20 +14,20 @@ export default function ProfileCard(props: {
   };
 
   return (
-    <div className="h-full w-[150px] md:w-[200px] bg-white transition-all m-4"> 
-      <div className="flex h-full w-[150px] md:w-[200px] flex-col items-center bg-white">
-        <Box
-          className="h-[150px] md:h-[200px] w-[150px] md:w-[200px] mb-4" 
-          bgImg={props.avatar}
-          bgSize="cover"
-          bgRepeat="no-repeat"
-          bgPosition="center"
-        />
-        <div className="text-start text-lg my-2 w-full text-primary">
-          {props.name}
-          <div className="whitespace-pre-wrap text-sm font-medium text-[#949494]">
-            {props.company}
-          </div>
+    <div className="h-auto w-[150px] md:w-[200px] bg-white transition-all m-4 flex flex-col items-center"> 
+      {/* 縮小圖片 */}
+      <Box
+        className="h-[120px] md:h-[160px] w-[120px] md:w-[160px] mb-4"
+        bgImg={props.avatar}
+        bgSize="cover"
+        bgRepeat="no-repeat"
+        bgPosition="center"
+      />
+      {/* 文字部分 */}
+      <div className="text-center text-lg w-full">
+        <div className="text-primary font-semibold">{props.name}</div>
+        <div className="whitespace-pre-wrap text-sm font-medium text-[#949494] mt-1">
+          {props.company}
         </div>
       </div>
     </div>
